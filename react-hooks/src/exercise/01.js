055874df61c38959d5d3533dc77c977fc3b99d65
Fenix4088 <fenix4088@gmail.com>
2022-01-15 15:53:24 +0200

    hooks: 01

--- react-hooks/src/exercise/01.js
+++ react-hooks/src/exercise/01.js
@@ -3,27 +3,27 @@
 
 import * as React from 'react'
 
-function Greeting() {
-  // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+function Greeting({initialName = ''}) {
+    const [name, setName] = React.useState(initialName);
 
-  function handleChange(event) {
-    // 🐨 update the name here based on event.target.value
-  }
+    function handleChange(event) {
+        const {value} = event.currentTarget;
+        setName(value);
+    }
 
-  return (
-    <div>
-      <form>
-        <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
-      </form>
-      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
-    </div>
-  )
+    return (
+        <div>
+            <form>
+                <label htmlFor="name">Name: </label>
+                <input value={name} onChange={handleChange} id="name"/>
+            </form>
+            {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+        </div>
+    )
 }
 
 function App() {
-  return <Greeting />
+    return <Greeting initialName={'Leonardo'}/>
 }
 
 export default App
