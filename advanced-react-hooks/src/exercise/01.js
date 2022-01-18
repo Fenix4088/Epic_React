ac16bbd6ea6cb59da01134895883a4973ade0aff
Fenix4088 <fenix4088@gmail.com>
2022-01-18 18:19:39 +0200

    advanced-hooks: 01

--- advanced-react-hooks/src/exercise/01.js
+++ advanced-react-hooks/src/exercise/01.js
@@ -1,23 +1,25 @@
 // useReducer: simple Counter
 // http://localhost:3000/isolated/exercise/01.js
 
-import * as React from 'react'
+import * as React from 'react';
+
+export const countReducer = (state, newState) => newState
 
 function Counter({initialCount = 0, step = 1}) {
-  // 🐨 replace React.useState with React.useReducer.
-  // 💰 React.useReducer(countReducer, initialCount)
-  const [count, setCount] = React.useState(initialCount)
+    // 🐨 replace React.useState with React.useReducer.
+    // 💰 React.useReducer(countReducer, initialCount)
+    const [count, setCount] = React.useReducer(countReducer, initialCount);
 
-  // 💰 you can write the countReducer function so you don't have to make any
-  // changes to the next two lines of code! Remember:
-  // The 1st argument is called "state" - the current value of count
-  // The 2nd argument is called "newState" - the value passed to setCount
-  const increment = () => setCount(count + step)
-  return <button onClick={increment}>{count}</button>
+    // 💰 you can write the countReducer function so you don't have to make any
+    // changes to the next two lines of code! Remember:
+    // The 1st argument is called "state" - the current value of count
+    // The 2nd argument is called "newState" - the value passed to setCount
+    const increment = () => setCount(count + step)
+    return <button onClick={increment}>{count}</button>
 }
 
 function App() {
-  return <Counter />
+    return <Counter/>
 }
 
 export default App
