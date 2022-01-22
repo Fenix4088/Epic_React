a88bbad4b7e6931f507153b2fa348b19438322fe
Fenix4088 <fenix4088@gmail.com>
2022-01-22 15:02:36 +0200

    advanced-hooks: (started) 03

--- advanced-react-hooks/src/exercise/03.js
+++ advanced-react-hooks/src/exercise/03.js
@@ -4,6 +4,7 @@
 import * as React from 'react'
 
 // 🐨 create your CountContext here with React.createContext
+const CountContext = React.createContext();
 
 // 🐨 create a CountProvider component here that does this:
 //   🐨 get the count state and setCount updater with React.useState
@@ -11,30 +12,40 @@ import * as React from 'react'
 //   🐨 return your context provider with the value assigned to that array and forward all the other props
 //   💰 more specifically, we need the children prop forwarded to the context provider
 
+export const CountProvider = ({children, props}) => {
+    const value = React.useState(0);
+
+    return <CountContext.Provider value={value} {...props}>
+        {children}
+    </CountContext.Provider>
+}
+
 function CountDisplay() {
-  // 🐨 get the count from useContext with the CountContext
-  const count = 0
-  return <div>{`The current count is ${count}`}</div>
+    // 🐨 get the count from useContext with the CountContext
+    const [count] = React.useContext(CountContext);
+    return <div>{`The current count is ${count}`}</div>
 }
 
 function Counter() {
-  // 🐨 get the setCount from useContext with the CountContext
-  const setCount = () => {}
-  const increment = () => setCount(c => c + 1)
-  return <button onClick={increment}>Increment count</button>
+    // 🐨 get the setCount from useContext with the CountContext
+    const [_, setCount] = React.useContext(CountContext);
+    const increment = () => setCount(c => c + 1)
+    return <button onClick={increment}>Increment count</button>
 }
 
 function App() {
-  return (
-    <div>
-      {/*
+    return (
+        <div>
+            {/*
         🐨 wrap these two components in the CountProvider so they can access
         the CountContext value
       */}
-      <CountDisplay />
-      <Counter />
-    </div>
-  )
+            <CountProvider>
+                <CountDisplay/>
+                <Counter/>
+            </CountProvider>
+        </div>
+    )
 }
 
 export default App
