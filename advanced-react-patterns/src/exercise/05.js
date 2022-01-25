9fbc99fdfca1029dedfb0b8442fb37609f9acb90
Fenix4088 <fenix4088@gmail.com>
2022-01-25 22:39:15 +0200

    advanced-react-patterns 05.2: The State Reducer Pattern(Invertion controll) state reducer action types

--- advanced-react-patterns/src/exercise/05.js
+++ advanced-react-patterns/src/exercise/05.js
@@ -6,12 +6,17 @@ import {Switch} from '../switch'
 
 const callAll = (...fns) => (...args) => fns.forEach(fn => fn?.(...args))
 
+const actionTypes = {
+  toggle: 'toggle',
+  reset: 'reset',
+}
+
 function toggleReducer(state, {type, initialState}) {
   switch (type) {
-    case 'toggle': {
+    case actionTypes.toggle: {
       return {on: !state.on}
     }
-    case 'reset': {
+    case actionTypes.reset: {
       return initialState
     }
     default: {
@@ -58,7 +63,7 @@ function App() {
   const clickedTooMuch = timesClicked >= 4
 
   function toggleStateReducer(state, action) {
-    if (action.type === 'toggle' && timesClicked >= 4) {
+    if (action.type === actionTypes.toggle && timesClicked >= 4) {
       return {on: state.on}
     }
     return toggleReducer(state, action)
