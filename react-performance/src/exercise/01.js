451e33ff05c615585d77398f31e6b653be14eec8
Fenix4088 <fenix4088@gmail.com>
2022-01-29 20:23:04 +0200

    react-performance 01: Code splitting

--- react-performance/src/exercise/01.js
+++ react-performance/src/exercise/01.js
@@ -2,11 +2,8 @@
 // http://localhost:3000/isolated/exercise/01.js
 
 import * as React from 'react'
-// 💣 remove this import
-import Globe from '../globe'
 
-// 🐨 use React.lazy to create a Globe component which uses a dynamic import
-// to get the Globe component from the '../globe' module.
+const Globe = React.lazy(() => import('../globe'))
 
 function App() {
   const [showGlobe, setShowGlobe] = React.useState(false)
@@ -34,12 +31,15 @@ function App() {
         />
         {' show globe'}
       </label>
-      <div style={{width: 400, height: 400}}>
-        {showGlobe ? <Globe /> : null}
-      </div>
+      <React.Suspense fallback={<div>Loading the Globe...</div>}>
+        <div style={{width: 400, height: 400}}>
+          {showGlobe ? <Globe /> : null}
+        </div>
+      </React.Suspense>
     </div>
   )
 }
+
 // 🦉 Note that if you're not on the isolated page, then you'll notice that this
 // app actually already has a React.Suspense component higher up in the tree
 // where this component is rendered, so you *could* just rely on that one.
