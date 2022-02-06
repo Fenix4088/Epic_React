e55d6053aca628de2b868d12b450a3f1b2107256
Fenix4088 <fenix4088@gmail.com>
2022-02-06 22:30:15 +0200

    testing-react-apps 02.1(Fix 2): simple test with React Testing Library: use @testing-library/jest-dom

--- testing-react-apps/src/__tests__/exercise/02.js
+++ testing-react-apps/src/__tests__/exercise/02.js
@@ -6,13 +6,13 @@ import {fireEvent, render} from '@testing-library/react'
 import Counter from '../../components/counter'
 
 test('counter increments and decrements when the buttons are clicked', () => {
-  const {container, debug} = render(<Counter />)
+  const {container} = render(<Counter />)
   const [decrement, increment] = container.querySelectorAll('button')
   const message = container.firstChild.querySelector('div')
 
   expect(message).toHaveTextContent('Current count: 0')
   fireEvent.click(increment)
-  debug(container)
+
   expect(message).toHaveTextContent('Current count: 1')
   fireEvent.click(decrement)
   expect(message).toHaveTextContent('Current count: 0')
