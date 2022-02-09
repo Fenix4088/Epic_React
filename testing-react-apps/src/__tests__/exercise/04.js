893386626e6cd5b11a90710d340ed856bbb650e8
Fenix4088 <fenix4088@gmail.com>
2022-02-09 21:02:56 +0200

    testing-react-apps 04.2: form testing: generate test data

--- testing-react-apps/src/__tests__/exercise/04.js
+++ testing-react-apps/src/__tests__/exercise/04.js
@@ -5,30 +5,35 @@ import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
+import faker from 'faker'
+
+const buildLoginForm = (overrides) => {
+  return {
+    username: faker.internet.userName(),
+    password: faker.internet.password(),
+    ...overrides
+  }
+}
 
 test('submitting the form calls onSubmit with username and password', () => {
 
-  // let submittedData = null
-  const expectedInputData = {
-    username: 'Bill',
-    password: '123'
-  }
+  const {username, password} = buildLoginForm()
 
   const handleSubmit = jest.fn();
 
     render(<Login onSubmit={handleSubmit}/>)
 
-    const {getByRole, getByLabelText, debug} = screen;
+    const {getByRole, getByLabelText} = screen;
 
     const usernameInput = getByLabelText(/username/gi);
     const passwordInput = getByLabelText(/password/gi);
     const submitBtn = getByRole('button', {name: /submit/gi})
 
-    userEvent.type(usernameInput, expectedInputData.username);
-    userEvent.type(passwordInput, expectedInputData.password);
+    userEvent.type(usernameInput, username);
+    userEvent.type(passwordInput, password);
     userEvent.click(submitBtn);
 
-  expect(handleSubmit).toHaveBeenCalledWith(expectedInputData);
+  expect(handleSubmit).toHaveBeenCalledWith({username, password});
   expect(handleSubmit).toHaveBeenCalledTimes(1);
 })
 
