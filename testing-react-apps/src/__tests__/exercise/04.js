6a5cd4f440d4506893c11c5cc6b2ebd85acfda9c
Fenix4088 <fenix4088@gmail.com>
2022-02-09 21:15:42 +0200

    testing-react-apps 04.4: form testing: use Test Data Bot

--- testing-react-apps/src/__tests__/exercise/04.js
+++ testing-react-apps/src/__tests__/exercise/04.js
@@ -5,19 +5,27 @@ import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
-import faker from 'faker'
+// import faker from 'faker';
+import {build, fake} from '@jackfranklin/test-data-bot';
 
-const buildLoginForm = (overrides) => {
-  return {
-    username: faker.internet.userName(),
-    password: faker.internet.password(),
-    ...overrides
-  }
-}
+// const buildLoginForm = (overrides) => {
+//   return {
+//     username: faker.internet.userName(),
+//     password: faker.internet.password(),
+//     ...overrides
+//   }
+// }
+
+const buildLoginForm = build({
+  fields: {
+    username: fake(f => f.internet.userName()),
+    password: fake(f => f.internet.password()),
+  },
+})
 
 test('submitting the form calls onSubmit with username and password', () => {
 
-  const {username, password} = buildLoginForm({password: '123'})
+  const {username, password} = buildLoginForm()
 
   const handleSubmit = jest.fn();
 
