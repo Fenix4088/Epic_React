25dad6c98bddd2b1042e99dfe3c4c03c19a17429
Fenix4088 <fenix4088@gmail.com>
2022-02-16 19:52:57 +0200

    testing-react-apps 08.2: testing custom hooks: setup function

--- testing-react-apps/src/__tests__/exercise/08.js
+++ testing-react-apps/src/__tests__/exercise/08.js
@@ -6,57 +6,69 @@ import {act, render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import useCounter from '../../components/use-counter'
 
- const UseCounterHookExample = () => {
-    const {count, increment, decrement} = useCounter();
+const UseCounterHookExample = () => {
+  const {count, increment, decrement} = useCounter()
 
-     return (
-         <div>
-             <div>Current count: {count}</div>
-             <button onClick={decrement}>Decrement</button>
-             <button onClick={increment}>Increment</button>
-         </div>
-     )
-
+  return (
+    <div>
+      <div>Current count: {count}</div>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={increment}>Increment</button>
+    </div>
+  )
 }
 
 test('exposes the count and increment/decrement functions', () => {
-    render(<UseCounterHookExample/>)
-    const {getByText} = screen;
-    const message = getByText(/Current count: \d+$/i);
-    // const [dec, inc] = getAllByRole('button');
-    const inc = screen.getByRole('button', {name: /increment/i})
-    const dec = screen.getByRole('button', {name: /decrement/i})
+  render(<UseCounterHookExample />)
+  const {getByText} = screen
+  const message = getByText(/Current count: \d+$/i)
+  // const [dec, inc] = getAllByRole('button');
+  const inc = screen.getByRole('button', {name: /increment/i})
+  const dec = screen.getByRole('button', {name: /decrement/i})
 
-    expect(message).toHaveTextContent('Current count: 0');
-    userEvent.click(inc);
-    expect(message).toHaveTextContent('Current count: 1');
-    userEvent.click(dec);
-    expect(message).toHaveTextContent('Current count: 0');
-
+  expect(message).toHaveTextContent('Current count: 0')
+  userEvent.click(inc)
+  expect(message).toHaveTextContent('Current count: 1')
+  userEvent.click(dec)
+  expect(message).toHaveTextContent('Current count: 0')
 })
 
-let result;
-const UseCounterHookExampleWithoutJSX = () => {
-    result = useCounter();
+export const setup = ({initialProps} = {}) => {
+  let result = {}
+  const UseCounterHookExampleWithoutJSX = () => {
+    Object.assign(result, useCounter(initialProps))
 
     return null
+  }
+
+  render(<UseCounterHookExampleWithoutJSX />)
+  return result
 }
 
+
+
 it('exposes the count and increment/decrement functions (without JSX)', () => {
-    let result;
-    const UseCounterHookExampleWithoutJSX = () => {
-        result = useCounter();
+  const result = setup();
 
-        return null
-    }
-    render(<UseCounterHookExampleWithoutJSX/>)
+  expect(result.count).toBe(0)
+  act(() => result.increment())
+  expect(result.count).toBe(1)
+  act(() => result.decrement())
+  expect(result.count).toBe(0)
+})
 
-    expect(result.count).toBe(0);
-    act(() => result.increment())
-    expect(result.count).toBe(1);
-    act(() => result.decrement())
-    expect(result.count).toBe(0);
+test('allows customization of the initial count', () => {
+  const result = setup({initialProps: {initialCount: 3}})
+  expect(result.count).toBe(3)
+})
 
-});
+test('allows customization of the step', () => {
+  const result = setup({initialProps: {step: 2}})
+  expect(result.count).toBe(0)
+  act(() => result.increment());
+  expect(result.count).toBe(2);
+  act(() => result.decrement())
+  expect(result.count).toBe(0)
+})
 
 /* eslint no-unused-vars:0 */
